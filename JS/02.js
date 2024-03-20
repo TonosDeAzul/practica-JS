02afc30023e4641e953e7455bb36b18e7ad4c826
TonosDeAzul <dg.wrq109@gmail.com>
2024-03-20 17:30:38 -0500

    avance 2

--- JS/02.js
+++ JS/02.js
@@ -4,6 +4,13 @@
 Escribir una función llamada calcularImpuestos que reciba dos argumentos numéricos: edad e ingresos. Si edad es igual o mayor a 18 y los ingresos son iguales o mayores a 1000 debe retornar ingresos * 40%. De lo contrario retornar 0.
 */
 
+let edad = parseInt(prompt("Ingrese la edad"));
+let ingresos = parseFloat(prompt("Ingrese los ingresos"));
+
 function calcularImpuestos(edad, ingresos) {
-  edad >= 18 && ingresos >= 1_000;
+  edad >= 18 && ingresos >= 1_000
+    ? console.log(`Debe pagar impuesto\n${ingresos * 0.4}`)
+    : console.log(`No debe pagar impuestos 0`);
 }
+
+calcularImpuestos(edad, ingresos);
